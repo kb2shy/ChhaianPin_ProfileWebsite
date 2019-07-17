b37fdef92710d5c0b2f7a989cef7875aaa6ac34e
kb2shy <chhaianpin@hotmail.com>
2019-07-17 13:40:42 -0700

    image added so far

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+
+// import Header from './Header';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Image src="img/banner2.jpg" fluid />
   )
 }
 
