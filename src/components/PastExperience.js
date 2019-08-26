d243e7060eab729468244b734637ff1393ec77b2
Chhaian Pin <chhaianpin@hotmail.com>
2019-08-26 10:50:17 -0700

    added sorting by name function to TechnicalSkills component

--- src/components/PastExperience.js
+++ src/components/PastExperience.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
     marginTop: 10,
+  },
+  paper: {
+    padding: 30,
   }
 })
 
@@ -15,8 +19,11 @@ const PastExperience = withStyles(styles)(({ classes }) => {
     <div className={classes.root}>
       <Container>
         <Typography variant="h4" align="center">
-          Past Experience
+          Portfolio
         </Typography>
+        <Paper className={classes.paper}>
+          <img></img>
+        </Paper>
       </Container>
     </div>
   )
--- src/components/TechnicalSkills.js
+++ src/components/TechnicalSkills.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import skills from '../content/skills';
+import React, { useState } from 'react';
+import skillsObject from '../content/skills';
 import { withStyles, Typography, Grid } from '@material-ui/core';
+import Switch from '@material-ui/core/Switch';
 import SkillCard from './SkillCard';
 
 const styles = theme => ({
@@ -12,11 +13,28 @@ const styles = theme => ({
 })
 
 const TechnicalSkills = withStyles(styles)(({ classes }) => {
+  const [sortByName, setSortByName] = useState(false);
+  const [skills, setSkills] = useState(skillsObject);
+
+  const toggleSortByName = () => {
+    setSortByName(!sortByName);
+    sortSkillsByName(skills);
+  }
+
+  const sortSkillsByName = (skills) => {
+    const result = skills.sort((a, b) => (a.skill > b.skill) ? 1 : -1)
+    setSkills(result);
+  }
+
   return (
     <div className={classes.root}>
       <Typography variant="h4" align="center" gutterBottom>
         Technical Skills
       </Typography>
+      <Switch checked={sortByName} onChange={toggleSortByName} value={sortByName}>
+        {console.log(sortByName)}
+        {console.log(skills)}
+      </Switch>
       <Grid container className={classes.grid} spacing={1}>
         {
           skills.map((skill, key) => <SkillCard skill={skill} key={key} />)
