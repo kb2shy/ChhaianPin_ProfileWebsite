f302b58b0b0d53afb4c9c52bd29674f7be2ea196
Chhaian Pin <chhaianpin@hotmail.com>
2019-08-26 16:13:18 -0700

    added star sorting on TechnicalSkills component

--- src/components/TechnicalSkills.js
+++ src/components/TechnicalSkills.js
@@ -3,6 +3,8 @@ import skills from '../content/skills';
 import { withStyles, Typography, Grid } from '@material-ui/core';
 import Switch from '@material-ui/core/Switch';
 import SkillCard from './SkillCard';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 const styles = theme => ({
   root: {
@@ -16,15 +18,28 @@ const TechnicalSkills = withStyles(styles)(({ classes }) => {
 
   const [skillsContent, setSkillsContent] = useState(skills);
   const [onSort, setOnSort] = useState(false);
+  const [onStars, setOnStars] = useState(false);
 
-  const handleChange = () => {
+  const handleChangeOnSort = () => {
     setOnSort(!onSort);
-    (onSort) ? setSkillsContent(skills) : sortSkillsContent();
+    (onSort) ? setSkillsContent(skills) : sortSkillsByName();
   }
 
-  const sortSkillsContent = () => {
+  const sortSkillsByName = () => {
     const result = skillsContent.concat().sort((a, b) => (a.skill < b.skill) ? -1 : 1)
-    setSkillsContent(result)
+    setSkillsContent(result);
+    setOnStars(false);
+  }
+
+  const handleChangeOnStars = () => {
+    setOnStars(!onStars);
+    (onStars) ? setSkillsContent(skills) : sortSkillsByStars();
+  }
+
+  const sortSkillsByStars = () => {
+    const result = skillsContent.concat().sort((a, b) => (a.rating > b.rating) ? -1 : 1);
+    setSkillsContent(result);
+    setOnSort(false);
   }
   
   return (
@@ -32,8 +47,15 @@ const TechnicalSkills = withStyles(styles)(({ classes }) => {
       <Typography variant="h4" align="center" gutterBottom>
         Technical Skills
       </Typography>
-      <Switch checked={onSort} onChange={handleChange}/>
-      <label>Sort by Skill Name</label>
+      <FormGroup row>
+        <FormControlLabel control={<Switch checked={onSort} onChange={handleChangeOnSort}/>}
+        label="Sort By Skill Name">
+        </FormControlLabel>
+        <FormControlLabel control={<Switch checked={onStars} onChange={handleChangeOnStars}/>}
+        label="Sort By Star Rating">
+        </FormControlLabel>
+      </FormGroup>
+      
       <Grid container className={classes.grid} spacing={1}>
         {
           skillsContent.map((skill, key) => <SkillCard skill={skill} key={key} />)
