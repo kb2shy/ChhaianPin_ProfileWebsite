706847f2a8944708929210ab7fb97ad8b72c5934
Chhaian Pin <chhaianpin@hotmail.com>
2019-08-28 09:19:50 -0700

    AppBar will display tabs or menu button on mobile and desktop browsers

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { withStyles } from '@material-ui/core/styles';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-// import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import Home from './Home';
 import Education from './Education';
@@ -28,17 +29,10 @@ const styles = theme => ({
   toolbarMargin: theme.mixins.toolbar
 });
 
-const TopMenu = withStyles(styles)(({ classes }) => (
-  <div className={classes.root}>
-    <AppBar position="fixed">
-      <Toolbar>
-        <Typography
-          variant="h6"
-          color="inherit"
-          className={classes.flex}
-        >
-          Chhaian's Website
-        </Typography>
+const TopMenu = withStyles(styles)(({ classes, width }) => {
+  const menu = () => {
+    if (isWidthUp('sm', width)) {
+      return (
         <Tabs aria-label="menu tabs">
           <Tab label="Home" />
           <Tab label="Education" />
@@ -46,12 +40,36 @@ const TopMenu = withStyles(styles)(({ classes }) => (
           <Tab label="Portfolio" />
           <Tab label="Contact Me" />
         </Tabs>
-      </Toolbar>
-    </AppBar>
-  </div>
-))
+      )
+    }
+    if (isWidthUp('xs', width)) {
+      return (
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+      )
+    }
+  }
 
-export default TopMenu;
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            color="inherit"
+            className={classes.flex}
+          >
+            Chhaian's Website
+          </Typography>
+          {menu()}
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+})
+
+export default withWidth()(TopMenu);
 
 {/* <Router>
 <Route exact path="/" component={Home} />
