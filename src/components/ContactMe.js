cbd4fc927fa84fe888af38d849a49ee53f4bc478
Chhaian Pin <chhaianpin@hotmail.com>
2019-08-28 16:53:40 -0700

    added LinkedIn page to ContactMe component

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -22,8 +22,13 @@ const ContactMe = withStyles(styles)(({classes}) => {
           <Typography variant="h4" align="center">
             Contact Me
           </Typography>
-          <Typography variant="h5" align="center">
+          <Typography variant="h6" align="center">
             Email: <a href="mailto:chhaian@gmail.com">Chhaian Pin</a><br />
+          </Typography>
+          <Typography variant="h6" align="center">
+            <a href="https://www.linkedin.com/in/chhaianpin/" target="_blank" rel="noopener noreferrer">
+              <img src="/icons/linkedin-24.png" alt="linkedin icon"></img>My LinkedIn Page
+            </a>
           </Typography>
           <Typography variant="subtitle2" align="center">
             Copyright © 2019, Chhaian Pin, All Rights Reserved.
